fe0e51619bf01930f83f9e8d89e88fa8e718695f
farooq13 <farukidris19@gmail.com>
2026-05-01 12:46:47 +0100

    config: configure db connection

--- src/config/env.ts
+++ src/config/env.ts
@@ -5,4 +5,5 @@ dotenv.config();
 export const config = {
   nodeEnv: process.env.NODE_ENV || 'development',
   port: process.env.PORT || 3000,
+  mongoUri: process.env.MONGO_URI || 'mongodb://localhost:27017/t-world-api',
 };
